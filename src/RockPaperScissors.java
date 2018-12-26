5a65c1edabc8528e64e694e3c5b0c8e4e70f5c1a
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2018-12-26 14:28:52 -0500

    Update RockPaperScissors.java

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,27 +1,8 @@
 /*
- * RockPaperScissors.java
- * Author: Cynthia He
- * Submission Date: September 27, 2018
- *
- * Purpose: This program lets the user play against a computer
+ * This program lets the user play against a computer
  * in a game of "Rock, Paper, Scissors" where the user has to choose either "rock",
  * "paper", or "scissors" and then show his/her choice to the computer at the same time.
  * The user's choice can win against the computer's choice or lose against.
- *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither
- * received nor given inappropriate assistance. I have not copied
- * or modified code from any source other than the course webpage
- * or the course textbook. I recognize that any unauthorized
- * assistance or plagiarism will be handled in accordance with
- * the University of Georgia's Academic Honesty Policy and the
- * policies of this course. I recognize that my work is based
- * on an assignment created by the Department of Computer Science
- * at the University of Georgia. Any publishing
- * or posting of source code for this project is strictly
- * prohibited unless you have written consent from the Department
- * of Computer Science at the University of Georgia.
  */
 
 import java.util.Scanner;
